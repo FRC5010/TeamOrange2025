4b35032341721e13e193f31f68a392d1a80bf6ef
CameronSkea <cameronskea@icloud.com>
2025-09-25 17:12:08 -0400

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC5010Example";
+  public static final String MAVEN_NAME = "TeamBlack2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-09-24 17:41:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1758750081528L;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "a5a7602d311560232663a0b03e3e2a5939ad101a";
+  public static final String GIT_DATE = "2025-09-24 17:41:55 EDT";
+  public static final String GIT_BRANCH = "CameronJr";
+  public static final String BUILD_DATE = "2025-09-24 18:47:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1758754021270L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
