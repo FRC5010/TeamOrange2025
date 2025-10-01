c3aaaef20ba3131bded543acf817343fddc91af8
Adamtheboss9 <adamtheboss78@gmail.com>
2025-10-01 18:36:15 -0400

    Fixing things

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,21 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamOrange2025";
   public static final String VERSION = "unspecified";
+<<<<<<< Updated upstream
   public static final int GIT_REVISION = 12;
   public static final String GIT_SHA = "898723135c43e2b6b26fb5dab1f67a42809cefd8";
   public static final String GIT_DATE = "2025-10-01 17:09:28 EDT";
   public static final String GIT_BRANCH = "6-swerve-updates";
   public static final String BUILD_DATE = "2025-10-01 17:49:41 EDT";
   public static final long BUILD_UNIX_TIME = 1759355381417L;
+=======
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "62384eb7bc8af02d13758a63300770f1b8b4ae17";
+  public static final String GIT_DATE = "2025-09-29 18:24:03 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-10-01 18:34:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1759358093386L;
+>>>>>>> Stashed changes
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
