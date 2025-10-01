9c5d9d2bf19492b7e87e08e8e61afe82afe84d90
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2025-10-01 17:05:08 -0400

    Add black_robot to robots.json

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC5010Example";
+  public static final String MAVEN_NAME = "TeamBlack2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "a5a7602d311560232663a0b03e3e2a5939ad101a";
-  public static final String GIT_DATE = "2025-09-24 17:41:55 EDT";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "6d6bed62ee19993841c33ec8571bc7d3e827072a";
+  public static final String GIT_DATE = "2025-10-01 17:02:59 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-09-25 20:26:00 EDT";
-  public static final long BUILD_UNIX_TIME = 1758846360959L;
+  public static final String BUILD_DATE = "2025-10-01 17:04:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1759352675342L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
