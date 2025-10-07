9b8a68b30d7564f2a003e74abf5faf07269fedb0
Curt <clrozeboom@gmail.com>
2025-10-07 11:17:43 -0400

    Restore buttercup config files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC5010Example";
+  public static final String MAVEN_NAME = "TeamOrange2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "7cb77ee59bae63f552afeda16867765ac1d22e2b";
-  public static final String GIT_DATE = "2025-10-05 14:47:51 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-10-07 10:38:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1759847910272L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 36;
+  public static final String GIT_SHA = "50d2b83e925802d16ddaeeb6d909a40b369005cc";
+  public static final String GIT_DATE = "2025-10-07 11:11:12 EDT";
+  public static final String GIT_BRANCH = "LobbinLoco";
+  public static final String BUILD_DATE = "2025-10-07 11:14:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1759850068153L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
