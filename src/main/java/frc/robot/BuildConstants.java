cc4813d1864a068ac635120f2dbbff831d33b152
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2025-11-05 18:58:00 -0500

    Working changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "abc18064dfa77b03ee13c3155b91b741322ade82";
   public static final String GIT_DATE = "2025-11-03 18:51:19 EST";
   public static final String GIT_BRANCH = "LobbinLoco";
-  public static final String BUILD_DATE = "2025-11-05 17:03:20 EST";
-  public static final long BUILD_UNIX_TIME = 1762380200517L;
+  public static final String BUILD_DATE = "2025-11-05 18:54:46 EST";
+  public static final long BUILD_UNIX_TIME = 1762386886850L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
