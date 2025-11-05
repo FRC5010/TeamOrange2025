d29150be6213f865b141e90bcfaaebcccee34d96
Adamtheboss9 <adamtheboss78@gmail.com>
2025-11-05 17:20:07 -0500

    Updated config

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamOrange2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 55;
-  public static final String GIT_SHA = "5191f67af851497efb1102ae57f66f9df801692b";
-  public static final String GIT_DATE = "2025-10-26 13:48:02 EDT";
+  public static final int GIT_REVISION = 64;
+  public static final String GIT_SHA = "abc18064dfa77b03ee13c3155b91b741322ade82";
+  public static final String GIT_DATE = "2025-11-03 18:51:19 EST";
   public static final String GIT_BRANCH = "LobbinLoco";
-  public static final String BUILD_DATE = "2025-10-27 18:16:19 EDT";
-  public static final long BUILD_UNIX_TIME = 1761603379283L;
+  public static final String BUILD_DATE = "2025-11-05 17:03:20 EST";
+  public static final long BUILD_UNIX_TIME = 1762380200517L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
