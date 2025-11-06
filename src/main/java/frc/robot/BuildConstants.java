8cc1959e8c62a6b317b078675a8c326b64b24381
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2025-11-06 18:48:18 -0500

    Swerve Config

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamOrange2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "cc4813d1864a068ac635120f2dbbff831d33b152";
-  public static final String GIT_DATE = "2025-11-05 18:58:00 EST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "1c8d580e734f314e6a42806c7905e2af6919e592";
+  public static final String GIT_DATE = "2025-11-06 12:35:06 EST";
   public static final String GIT_BRANCH = "LobbinLoco";
-  public static final String BUILD_DATE = "2025-11-06 12:31:28 EST";
-  public static final long BUILD_UNIX_TIME = 1762450288700L;
+  public static final String BUILD_DATE = "2025-11-06 18:41:31 EST";
+  public static final long BUILD_UNIX_TIME = 1762472491889L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
