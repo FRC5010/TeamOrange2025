1c8d580e734f314e6a42806c7905e2af6919e592
Curt <clrozeboom@gmail.com>
2025-11-06 12:35:06 -0500

    Use copied YAGSL

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamOrange2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "abc18064dfa77b03ee13c3155b91b741322ade82";
-  public static final String GIT_DATE = "2025-11-03 18:51:19 EST";
+  public static final int GIT_REVISION = 66;
+  public static final String GIT_SHA = "cc4813d1864a068ac635120f2dbbff831d33b152";
+  public static final String GIT_DATE = "2025-11-05 18:58:00 EST";
   public static final String GIT_BRANCH = "LobbinLoco";
-  public static final String BUILD_DATE = "2025-11-05 18:54:46 EST";
-  public static final long BUILD_UNIX_TIME = 1762386886850L;
+  public static final String BUILD_DATE = "2025-11-06 12:31:28 EST";
+  public static final long BUILD_UNIX_TIME = 1762450288700L;
   public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
